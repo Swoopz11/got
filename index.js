bbeecacce324baeaa62bea4d7994bcf73345a7e3
Hunter Birch <hunter.macka.birch@gmail.com>
2022-05-25 12:30:33 -0600

    Jamie's first attack

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+
 let jonSnowAttack = 25;
-let jamieLannisterAttack = 35;
+let johSnowHealth = 100;
+
+
+let jamieLannisterAttack = 107;
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log('Jon Snow has better attack than Jamie Lannister')
@@ -7,4 +11,11 @@ if (jonSnowAttack > jamieLannisterAttack) {
     console.log('Jamie Lannister has better attack than Jon Snow')
 } else {
     console.log('Jon Snow and Jamie Lannister have the same attack')
+}
+
+if (johSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain")
+} else {
+    johSnowHealth -= jamieLannisterAttack;
+    console.log(`Jon Snow's health is down to ${johSnowHealth}`);
 }
